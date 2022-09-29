0dd14550df3576daac7c2b7106f004de487c4219
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-29 17:58:17 -0300

    exercicio 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const value = data.species.find((specie) => specie.name === animal).residents;
+  return value.every((e) => e.age >= age);
 }
-
+console.log(getAnimalsOlderThan('lions', 7));
 module.exports = getAnimalsOlderThan;
