dcb3689d7bbca1fd5d33f30c17f8da7015c8dba4
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-30 17:16:12 -0300

    exercicio 5 terminado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,13 +3,24 @@ const data = require('../data/zoo_data');
 const { species } = data;
 function countAnimals(animal) {
   const obj = {};
-  species.forEach((element) => {
-    obj[element.name] = element.residents.length;
-  });
+  species.forEach((element) => { obj[element.name] = element.residents.length; });
+
+  // if (Object.keys(animal).includes('sex')) {
+  //   obj[element.name] = element.residents.filter((e) => e.sex === animal.sex).length;
+  //   return;
+  // }
+  // if (Object.keys(animal).includes('specie')) {
+  //   obj[element.name] = element.residents.length;
+  // }
   if (animal === undefined) {
     return obj;
   }
-  return obj[animal.specie];
+  if (Object.keys(animal).includes('sex')) {
+    return species.find((element) => element.name === animal.specie).residents
+      .filter((e) => e.sex === animal.sex).length;
+  }
+  return species.find((element) => element.name === animal.specie).residents.length;
 }
-console.log(countAnimals({ specie: 'penguins' }));
+
+console.log(countAnimals());
 module.exports = countAnimals;
