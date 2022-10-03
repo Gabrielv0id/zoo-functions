4a0e0aafb765f0fbbbd7a27193c4d08dd91793ac
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-03 15:26:55 -0300

    exercicio 8 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,46 @@
 const data = require('../data/zoo_data');
 
+const {prices} = data;
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = {
+    adult: 0,
+    senior: 0,
+    child: 0,
+  };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      obj.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      obj.adult += 1;
+    } else if (element.age >= 50) {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqu
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const valores = countEntrants(entrants);
+  let sum = 0;
+  Object.values(valores).forEach((element, index) => {
+    sum += element * Object.values(prices)[index];
+    return sum;
+  });
+  return sum;
 }
-
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
