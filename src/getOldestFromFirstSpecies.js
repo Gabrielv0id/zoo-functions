4054ae1f0759e9dfd7b908b7d9586e3b15506aa5
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-12 08:58:34 -0300

    att da 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const worker = employees.find((employee) => employee.id === id);
+  const animal = worker.responsibleFor[0];
+  const specie = species.find((element) => animal.includes(element.id));
+  const objetoAnimais = specie.residents;
+  let idade = 0;
+  let maisVelho;
+  objetoAnimais.forEach((resident) => {
+    if (idade < resident.age) {
+      idade = resident.age;
+      maisVelho = resident;
+    }
+  });
+  const oldest = Object.values(maisVelho);
+  return oldest;
 }
-
 module.exports = getOldestFromFirstSpecies;
